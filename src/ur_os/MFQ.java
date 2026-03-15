31ac91fb4ace4a23614c9f790a7025053a0f8904
dav <124942043+merequetenguee@users.noreply.github.com>
2026-03-15 16:31:29 -0500

    w

--- src/ur_os/MFQ.java
+++ src/ur_os/MFQ.java
@@ -34,7 +34,54 @@ public class MFQ extends Scheduler{
         
     @Override
     public void addProcess(Process p){
-       //Overwriting the parent's addProcess(Process p) method may be necessary in order to decide what to do with process coming from the CPU.
+        System.out.println(p.getState());
+        System.out.println("p sch:"+p.currentScheduler);
+        System.out.println("currentsch:"+currentScheduler);
+        if (p.getState()==ProcessState.NEW){
+            System.out.println("in new");
+            System.out.println("Pid:"+p.getPid());
+            p.currentScheduler=0;
+
+            schedulers.get(0).addProcess(p);
+            this.currentScheduler=0;
+        } else if (p.getState()==ProcessState.CPU) {
+            //into second
+            System.out.println("in second");
+            System.out.println("Pid:"+p.getPid());
+            if (p.currentScheduler<schedulers.size()-1){
+                schedulers.get(p.currentScheduler+1).addProcess(p);
+                p.currentScheduler++;
+
+
+            }
+            System.out.println("loaded");
+
+
+
+
+
+
+
+
+        } else if (p.getState()==ProcessState.IO){
+            System.out.println("in IO");
+            System.out.println("Pid:"+p.getPid());
+            p.currentScheduler=0;
+            if  (!(p.currentScheduler>this.currentScheduler)){
+                this.currentScheduler=p.currentScheduler;
+            }
+
+            this.schedulers.get(p.currentScheduler).processes.addFirst(p);
+
+
+
+        }
+        while (schedulers.get(this.currentScheduler).isEmpty() && this.currentScheduler<schedulers.size()-1){
+            this.currentScheduler=this.currentScheduler+1;
+
+        }
+        System.out.println("currentsch:"+this.currentScheduler);
+        //Overwriting the parent's addProcess(Process p) method may be necessary in order to decide what to do with process coming from the CPU.
         
     }
     
@@ -45,6 +92,13 @@ public class MFQ extends Scheduler{
    
     @Override
     public void getNext(boolean cpuEmpty) {
+        System.out.println(this.currentScheduler);
+        System.out.println("cpu empty:"+cpuEmpty);
+        //if (schedulers.get(currentScheduler).isEmpty()){
+            //return;
+        //}
+
+       schedulers.get(this.currentScheduler).getNext(cpuEmpty);
         //Suggestion: now that you know on which scheduler a process is, you need to keep advancing that scheduler. If it a preemptive one, you need to notice the changes
         //that it may have caused and verify if the change is coherent with the priority policy for the queues.
   
